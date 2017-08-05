0fead35b7bfdbeb527f827d1738e61f251e33fd9
Dhruv Behl <dhruvbehl10@gmail.com>
2017-08-05 19:07:37 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
     description="Displays results and fixtures for the"
                 "FC Barcelona matches",
     scripts=["fcb.py"],
+    install_requires=[
+        "beautifulsoup4>=4.6.0",
+        "click>=6.7",
+        "requests>=2.18.1",
+        "termcolor>=1.1.0"
+    ],
     entry_points={
         "console_scripts": [
             "fcb=fcb:main",
